68459d3fb11615a2e7d9cdb984f51b0f26427142
lain <gilgamesh06lg@gmail.com>
2025-01-04 22:29:48 -0500

    Modelo que representa la entidad intermedia ProveedorProducto.

---
+++ Backend/inventario/models/ProveedorProducto.py
@@ -0,0 +1,18 @@
+"""
+Esta clase representa la Entidad ProveedorProducto
+en la base de datos.
+"""
+
+from sqlalchemy import Column, Integer,ForeignKey
+from ..config.db import Base
+from sqlalchemy.orm import relationship
+
+class ProveedorProducto(Base):
+    __tablename__="ProveedorProducto"
+
+    provedor_id = Column(Integer,ForeignKey('Proveedor.id'),primary_key=True,nullable=False)
+    producto_id = Column(Integer,ForeignKey('Producto.id'),primary_key=True,nullable=False)
+
+    # Relaciones 
+    proveedor = relationship("Provedor", back_populates="productos")
+    producto = relationship("Producto", back_populates="proveedores")
