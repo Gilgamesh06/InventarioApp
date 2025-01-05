0049521d76fbf963ec955e6571e4e12b2e63d568
lain <gilgamesh06lg@gmail.com>
2025-01-04 22:29:26 -0500

    Modelo que representa la entidad Producto.

---
+++ Backend/inventario/models/Producto.py
@@ -0,0 +1,26 @@
+"""
+Esta clase representa la Entidad Producto
+en la base de datos.
+"""
+
+from sqlalchemy import Column, Integer, String, Float
+from ..config.db import Base
+from sqlalchemy.orm import relationship
+
+class Producto(Base):
+    __tablename__="Producto"
+
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    codigo = Column(String(12), nullable=False, unique=True)
+    talla = Column(String(5),nullable=False)
+    nombre = Column(String(50),nullable=False)
+    tipo = Column(String(100),nullable=False)
+    valor_unid = Column(Float,nullable=False)
+    valor_venta = Column(Float,nullable=False)
+
+    # Relacion uno a muchos con ProveedorProducto
+    proveedores = relationship("Proveedor", secondary="ProveedorProducto",back_populates="productos")
+    # Relacion uno a muchos con MermaProducto
+    mermas = relationship("Merma", secondary="MermaProducto",back_populates="productos")
+    # Relacion uno a uno Producto
+    inventario = relationship("Inventario",back_populates="producto")
