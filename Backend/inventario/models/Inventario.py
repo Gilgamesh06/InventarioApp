0fbde22048a19f64681c19f770817fef550357c9
Gilgamesh06 <gilgamesh06lg@gmail.com>
2025-01-12 20:27:04 -0500

    Actualizacion de la clase Inventario cambio de la manera en que se define las relaciones.

--- Backend/inventario/models/Inventario.py
+++ Backend/inventario/models/Inventario.py
@@ -3,14 +3,14 @@ Esta clase representa la Entidad Inventario
 en la base de datos.
 """
 
-from sqlalchemy import Column, Integer, DATE
-from ..config.db import Base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, DATE, ForeignKey
+from config.db import Base
+from sqlalchemy.orm import relationship 
 
 class Inventario(Base):
     __tablename__="Inventario"
 
-    producto_id = Column(Integer,primary_key=True,unique=True,nullable=False)
+    producto_id = Column(Integer, ForeignKey('Producto.id'), primary_key=True ,unique=True,nullable=False)
     cantidad = Column(Integer,nullable=False)
     fecha = Column(DATE)
 
